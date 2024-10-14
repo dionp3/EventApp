cf73de4d40f82b3d7e33e4415c76d9f5743c3274
dionp3 <donpr2003@gmail.com>
2024-10-14 12:16:31 +0800

    Update Bottom Nav

--- app/src/main/java/com/dicoding/event/ui/notifications/FinishedFragment.kt
+++ app/src/main/java/com/dicoding/event/ui/notifications/FinishedFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.dicoding.event.databinding.FragmentNotificationsBinding
+import com.dicoding.event.databinding.FragmentFinishedBinding
 
-class NotificationsFragment : Fragment() {
+class FinishedFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentFinishedBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val finishedViewModel =
+            ViewModelProvider(this).get(FinishedViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentFinishedBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        finishedViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
